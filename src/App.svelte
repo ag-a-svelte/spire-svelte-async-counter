2ccc8fb64ed97c8d51bbb36428a3bcbc6f0ac2d4
Jitesh Doshi <jitesh@spinspire.com>
2020-05-14 15:38:29 -0400

    convert "loading" from local var to Svelte store

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,7 @@
 <script>
   import Spinner from "./Spinner.svelte";
-  let loading = 0;
+  import { writable } from "svelte/store";
+  let loading = writable(0);
   let delay = 1000;
   const url = "http://api.icndb.com/jokes/random";
   $: delayed_url = `http://slowwly.robertomurray.co.uk/delay/${delay}/url/${url}`;
@@ -16,10 +17,10 @@
      the wrapped function and decrements it after. */
   function wrap(f) {
     return async (...a) => {
-      ++loading;
+      loading.update(n => n + 1);
       // pass the same args
       const result = await f(...a);
-      --loading;
+      loading.update(n => n - 1);
       return result;
     };
   }
@@ -36,10 +37,12 @@
 
     <!-- to get a bigger spinner, increase the font size -->
     <div style="font-size: 300%">
-      <Spinner {loading} />
+      <Spinner loading={$loading} />
     </div>
     <input bind:value={delay} type="number" step={1000} min={0} />
-    <button on:click={wrap(fetch_joke)}>Fetch Joke</button>
+    <button on:click={wrap(fetch_joke)} disabled={$loading}>
+      Fetch Joke
+    </button>
     <h2>The Amazing Chuck Norris!</h2>
     <p>
       {#if joke}{joke}{:else}...{/if}
