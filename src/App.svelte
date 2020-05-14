03e2a04706494a989a0b30a64ade7d7c8b661a8e
Jitesh Doshi <jitesh@spinspire.com>
2020-05-14 15:24:10 -0400

    naive implementation: show spinner active during AJAX

--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,14 @@
 <script>
   import Spinner from "./Spinner.svelte";
-  let loading = 1;
+  let loading = 0;
   let delay = 1000;
   const url = "http://api.icndb.com/jokes/random";
   $: delayed_url = `http://slowwly.robertomurray.co.uk/delay/${delay}/url/${url}`;
   let joke = "";
   async function fetch_joke() {
+    ++loading;
     const response = await fetch(delayed_url);
+    --loading;
     const json = await response.json();
     joke = json.value.joke;
   }
@@ -21,16 +23,11 @@
     <img src="favicon.png" alt="favicon" />
     <h1>Svelte App</h1>
 
-    <div>
-      <input type="checkbox" bind:checked={loading} />
-      loading?
-      <Spinner {loading} />
-    </div>
     <!-- to get a bigger spinner, increase the font size -->
     <div style="font-size: 300%">
       <Spinner {loading} />
     </div>
-    <input bind:value={delay} type="number" step={1000} min={0}>
+    <input bind:value={delay} type="number" step={1000} min={0} />
     <button on:click={fetch_joke}>Fetch Joke</button>
     <h2>The Amazing Chuck Norris!</h2>
     <p>
